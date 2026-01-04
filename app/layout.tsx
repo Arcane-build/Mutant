a93ff09186a71823990125a9fb6175ee1811d941
B-Rohan-Rao <raorohan060@gmail.com>
2026-01-04 21:01:54 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Mutant',
   description: 'Speak once. It executes.',
+  icons: {
+    icon: '/faviconLogo.svg',
+  },
 };
 
 export default function RootLayout({
